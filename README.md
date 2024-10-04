ffd5aecc72b3d6e4eee4f1f834c6c166defd2700
ssihvola <fytapes@proton.me>
2024-10-04 09:49:44 +0300

    Edit README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a script I created for my personal use. I needed a way to rename my video files according to date and time the file is created. Manual typing is of course not an option when there are hundreds, is not thousands of files.
 
-Before doing any real-life renaming, it is important to test that the script works as expected. I chose Robot Framework as my testing tool, because of it's OperatingSystem library.
+Before doing any real-life renaming, it is good to test that the script works as expected. I chose Robot Framework as my testing tool, because of it's OperatingSystem library.
 
 ## Usage
 
@@ -16,7 +16,3 @@ pip install robotframework``
 
 - Python script to keep my personal files in order
 - Tests: Robot Framework creates dummy folders with dummy files with random names, runs the script and checks that files are renamed accordingly
-
-## Todo (bugs)
-
-- Script ignores files that are created at exactly the same time, turns them into one or something
--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import time
 def main():
 	# rename the folder variable when doing real-life renaming
 	folder = "files"
-	# create a list for the files
-	files = []
+	# initialize a number for each individual file
 	file_number = 1
 
 	# add all the files to the list with their creation data
@@ -14,34 +13,21 @@ def main():
 		# get the full file path
 		file_path = os.path.join(folder, filename)
 
-		#ensure it's a file
+		# ensure it's a file
 		if os.path.isfile(file_path):
 			# get file creation time
 			creation_time = os.path.getctime(file_path)
 			# format the creation time as a timestamp
 			timestamp = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(creation_time))
-			print(timestamp)
 			# get the file extension
 			file_name, file_extension = os.path.splitext(filename)
-			print(file_name)
-			print(file_extension)
-			# create the new file name & add a number also to make sure that each file is named individually
+			# create the new file name & add a number to make sure that each file is named individually
 			new_filename = f"{timestamp}-{file_number}{file_extension}"
 			file_number += 1
-			print(new_filename)
 			# full path for the new file name
 			new_file_path = os.path.join(folder, new_filename)
-			print(new_file_path)
 			# rename the file
 			os.rename(file_path, new_file_path)
-			print(file_path)
-			print(new_file_path)
-			# add file to the list
-			files.append(file_path)
-			print(files)
-			
-
-	print(files)
 
 
 if __name__ == '__main__':
